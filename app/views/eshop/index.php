80a6fb4ee49d781ef33c65a4253944e7eebac9ad
Yacine Mechlaoui <yacine.mechlaoui@esprit.tn>
2023-11-26 21:22:00 +0100

    product management integrated with user management

---
+++ app/views/eshop/index.php
@@ -0,0 +1,33 @@
+<?php $this->view("header",$data); ?>
+
+<?php $this->view("slider",$data); ?>
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				
+				<?php $this->view("sidebar.inc",$data); ?>
+				
+				<div class="col-sm-9 padding-right">
+					<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Featured Items</h2>
+						
+						<?php if(is_array($ROWS)): ?>
+						<?php foreach($ROWS as $row): ?>
+
+							<?php $this->view("product.inc",$row); ?>
+
+						<?php endforeach; ?>
+						<?php endif; ?>
+
+					</div><!--features_items-->
+					
+					
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	
+<?php $this->view("footer",$data); ?>
+
